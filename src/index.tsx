f95bd05e9e6b7a0cf3baba43d8d2a7127420df21
Konstantin.Sch <57962038+KonstantinScherbina@users.noreply.github.com>
2023-03-19 18:27:05 +0200

    basename

--- src/index.tsx
+++ src/index.tsx
@@ -14,13 +14,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
-    <HashRouter>
+    <BrowserRouter basename="/">
+      {/* <HashRouter> */}
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
-    {/* </BrowserRouter> */}
+      {/* </HashRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
