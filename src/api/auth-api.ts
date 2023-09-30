750410f4ccba22a1911444b79ec5ce639b4ab5dd
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-09-30 16:46:32 +0300

    trying

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -1,11 +1,11 @@
 import axios from "axios"
-import { IapiResponseResult } from "../types/types";
+import {IapiResponseResult} from "../types/types";
 
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
-    headers: { "API-KEY": "a11c1b6e-985d-42c3-9af2-d65c3bcf4ccb" }
+    headers: {"API-KEY": "a11c1b6e-985d-42c3-9af2-d65c3bcf4ccb"}
 });
 
 
@@ -13,25 +13,31 @@ export const authAPI = {
     // fetch autorized or not on server
     authAPIMe(): Promise<IapiResponseResult> {
         return instance.get(`auth/me`)
-            .then(response => { return response.data })
+            .then(response => {
+                return response.data
+            })
     },
 
     authAPILogIn(email: string, password: string, rememberMe: boolean = false): Promise<IapiResponseResult> {
-        debugger
-        return instance.post(`auth/login`, { email, password, rememberMe })
-            .then(response => { return response.data })
+        return instance.post(`auth/login`, {email, password, rememberMe})
+            .then(response => {
+                return response.data
+            })
     },
 
     // unAuthorizeing from server
     authAPILogOut(): Promise<IapiResponseResult> {
-        debugger
         return instance.delete(`auth/login`)
-            .then(response => { return response.data })
+            .then(response => {
+                return response.data
+            })
     },
 
     // fetch captcha image from server
     getCaptchaUrl(): Promise<string> {
         return instance.get(`security/get-captcha-url`)
-            .then(response => { return response.data })
+            .then(response => {
+                return response.data
+            })
     }
 }
